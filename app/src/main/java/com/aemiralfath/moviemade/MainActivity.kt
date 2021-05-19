a640388154f76c461a5a3f1eececc96edd7285b4
criptdestroyer <ahmademir1201@gmail.com>
2021-05-19 14:40:10 +0700

    add favorite

--- app/src/main/java/com/aemiralfath/moviemade/MainActivity.kt
+++ app/src/main/java/com/aemiralfath/moviemade/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.aemiralfath.moviemade
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -53,8 +55,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 title = getString(R.string.app_name)
             }
             R.id.nav_favorite -> {
-//                val uri = Uri.parse("moviemade://favorite")
-//                startActivity(Intent(Intent.ACTION_VIEW, uri))
+                val uri = Uri.parse("moviesapp://favorite")
+                startActivity(Intent(Intent.ACTION_VIEW, uri))
             }
         }
 
---
+++ favorite/src/main/java/com/aemiralfath/favorite/FavoriteModule.kt
@@ -0,0 +1,8 @@
+package com.aemiralfath.favorite
+
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val favoriteModule = module {
+    viewModel { FavoriteViewModel(get()) }
+}
